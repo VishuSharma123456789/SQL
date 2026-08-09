54005de35c80184aa177c1f7c038bc0685f4bdf7
VishuSharma123456789 <sharmavishu5832@gmail.com>
2026-08-10 00:06:20 +0530

    Add files via upload

---
+++ 04_case_sensitive_database.sql
@@ -0,0 +1,11 @@
+-- Creating a Case-Sensitive database
+
+CREATE DATABASE Namaste_SQL_2
+COLLATE SQL_Latin1_General_CP1_CS_AS;
+GO
+
+
+-- Switching to the newly created database
+
+USE Namaste_SQL_2;
+GO
